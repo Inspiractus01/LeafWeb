b418bcef4cb4c1b1d22091439bb75657fbb1c774
Michal <muchamichal@proton.me>
2024-02-05 20:26:02 +0100

    pridal som a vylepšil backstory leafloopu about page

--- src/components/About/aboutindex.tsx
+++ src/components/About/aboutindex.tsx
@@ -6,7 +6,7 @@ import "../styles.css";
 const ColorizedDiv = styled.div`
   position: relative;
   color: white;
-  height: 100vh;
+  height: 3000px;
   display: flex;
   align-items: top;
   justify-content: center;
@@ -28,7 +28,7 @@ const Title = styled.h1`
 `;
 const Title2 = styled.h1`
   margin: 0;
-  padding-top: 2vh;
+  padding-top: 3vh;
   font-family: "Regular-R";
   font-size: 2em;
   z-index: 1;
@@ -55,7 +55,7 @@ const Div1 = styled.div`
   justify-content: center; /* Add this line to center content horizontally */
   align-items: center; /* Optionally, you can also center vertically */
 
-  flex: 0.7;
+  flex: 0.3;
   display: flex;
   flex-direction: row;
   background-color: transparent;
@@ -153,10 +153,23 @@ const Button2 = styled(motion.button)`
 `;
 
 const Image = styled.img`
+  margin-top:20px;
   object-fit: cover;
   border: 2px solid #363434;
   width: 135vh;
   height: 45vh;
+  border-radius: 0px;
+
+  @media (max-width: 900px) {
+    width: 100%;
+    height: 30%;
+  }
+`;
+const Image1 = styled.img`
+  margin-top:20px;
+  object-fit: cover;
+  border: 2px solid #363434;
+  width: 30vh;
   border-radius: 0px;
 
   @media (max-width: 900px) {
@@ -181,6 +194,7 @@ const Div3 = styled.div`
 `;
 
 const Text = styled.h1`
+  width:80%;
   font-family: "Regular-R";
   padding-top: 5vh;
   font-size: 1em;
@@ -225,10 +239,29 @@ const Aboutpage = () => {
         </Div1>
 
         <Div2>
-          <Title>O nás</Title>
+        <Title>Tvoja AI Rastlinná Encyklopédia!</Title>
+        <Text1>V LeafLoop veríme v úžasný svet rastlín a chceme vám pomôcť objavovať ho jednoduchým a interaktívnym spôsobom. Naša platforma je viac než len encyklopédia - je to komunita nadšencov, ktorí zdieľajú svoju vášeň pre rastliny.</Text1>
+        <Image src="https://cdn.discordapp.com/attachments/1096191061015150754/1200639561819242606/LeafLoop.png?ex=65c6e9ba&is=65b474ba&hm=606eb5256c1e4db441e8d17c97618c3f44455a0fad876bc60ebcba49ec9b6447&"></Image>
+        <Title2>Naše Kúzlo:</Title2>
+        <Text>
+      LeafLoop je najväčšia knižnica rastlín na svete, generovaná umelou inteligenciou. Momentálne pracujeme na vývoji vlastnej AI, ktorá bude ešte presnejšia a účinnejšia. Vzhľadom na dynamiku sveta rastlín sa môžu vyskytnúť chyby, no s vašou pomocou a spoluprácou našej komunity stále zdokonaľujeme naše služby.
+      <br></br> <br></br> <strong>Ako to funguje? 🌱:</strong> <br></br>Jednoducho! Ak nenájdeš svoju rastlinu v našej databáze, máš možnosť ju pridať ty sám. Týmto jednoduchým krokom prispievaš k rozširovaniu našej encyklopédie a pomáhaš ostatným milovníkom rastlín objaviť nové druhy.
+        </Text>
+        <Title2>Prečo LeafLoop:</Title2>
+        <Text>
+        <u>⚪Otvorená Databáza Rastlín:</u> LeafLoop ponúka prístup k otvorenej databáze rastlín pre každého. Sme presvedčení, že spolupráca a zdieľanie informácií vedie k obohacovaniu našej znalostnej bázy.
+  <br></br><br></br>
+  <u>⚪Interaktívny Chatbot:</u> Náš chatbot je tu pre vás, aby odpovedal na vaše otázky a poskytol vám rady ohľadom starostlivosti o rastliny.
+  <br></br><br></br>
+  <u>⚪Komunitná Spolupráca:</u> LeafLoop nie je len databáza, je to živá komunita. Každý príspevok a pridaná rastlina robia našu encyklopédiu ešte bohatšou. Naša komunita tvorí srdce LeafLoop. S vašou aktívnou účasťou a zdieľaním znalostí prispejete k neustálemu rozvoju našej platformy.
+  <br></br><br></br>
+  <u>⚪Open API:</u> Poskytujeme otvorené API, ktoré umožňuje vývojárom a nadšencom pristupovať k našej databáze rastlín a integrovať ju do vlastných projektov. Tým podporujeme otvorenosť a inovácie vo svete rastlín.
+</Text>
+<Image src="https://cdn.discordapp.com/attachments/1096191061015150754/1204145357542653993/LeafLoop_2.png?ex=65d3aac1&is=65c135c1&hm=82c5efb82b81d90e48170d277e548d5703a33c7d41be12a6fb48f1c13760d15e&"></Image>
+
+          
+          <Title2>Ako sa tento zelený projekt vznikol?</Title2>
           <Text>Kto stojí za vytvorením LeafLoopom?</Text>
-          <Image src="https://cdn.discordapp.com/attachments/1096191061015150754/1200639561819242606/LeafLoop.png?ex=65c6e9ba&is=65b474ba&hm=606eb5256c1e4db441e8d17c97618c3f44455a0fad876bc60ebcba49ec9b6447&"></Image>
-          <Title2>Ako sa tento zelený projekt začal?</Title2>
           <Text1>
             "Chceš počuť príbeh o vzniku nášho zeleného projektu? Dovoľ mi, aby
             som ťa zaviedol späť na začiatok. Leafloop nevznikol len ako
@@ -239,12 +272,28 @@ const Aboutpage = () => {
             technológií s takýmto dôležitým aspektom života som začal projekt
             Leafloop. Spolu so svojím priateľom Palom sme starostlivo rozvíjali
             túto myšlienku, a dnes sme svedkami úspechu našej spoločnej
-            inovácie, ktorá vznikla z lásky k fascinujúcemu svetu rastlín." -
-            Michal
+            inovácie, ktorá vznikla z nadšenia k fascinujúcemu svetu rastlín." -
+            Michal Mucha
           </Text1>
+
+  <Title2>Naša Podpora:</Title2>
+<Text1>
+  Tento projekt si všimla renomovaná organizácia PlantNet, ktorá sa zameriava na rozvoj a podporu iniciatív spojených s rastlinami. Ich entuziazmus pre náš projekt LeafLoop nás potešil, a preto sme hrdí, že sme získali ich podporu.
+  <br></br><br></br>
+  PlantNet rozpoznal potenciál nášho projektu a jeho prínos pre komunitu milovníkov rastlín. S ich podporou sme získali prístup k ich API na detekciu rastlín z fotky, čo umožňuje našim užívateľom ešte viac interakcie a pohodlia pri identifikácii rastlín prostredníctvom LeafLoop.
+  <br></br><br></br>
+  Chceme vyjadriť veľkú vďaku PlantNet za ich dôveru a podporu. Spoločne budeme pokračovať v rozvoji a poskytovaní kvalitných služieb pre všetkých milovníkov rastlín.
+</Text1>
+<Image1 src="https://www.digitalcitizen.life/wp-content/uploads/2020/10/plantnet.png"></Image1>
+<Text>
+<Title2>Pripojte sa na náš Discord server</Title2>
+    <b></b>Chcete sa aktívne zapojiť do našej komunity a zdieľať svoju lásku k rastlinám? Pripojte sa k nášmu Discord serveru, kde môžete komunikovať s ostatnými nadšencami, získavať rady a zdieľať svoje skúsenosti!
+  </Text>
+  <a href="https://discord.gg/Sn3UvQBMef" className="discord-button">
+    Pripojte sa na Discord
+  </a>
         </Div2>
       </ColorizedDiv>
-      <ColorizedDiv>\</ColorizedDiv>
     </>
   );
 };
